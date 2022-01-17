e1f9c00d518e2647b31fa4486361d99b9a036633
Jorgen-Jr <Jorgen-Jr@users.noreply.github.com>
2022-01-17 17:46:10 +0000

    deploy: 09b7a6fc4d7217977a415b4e1fb27f82870f669b

---
+++ _next/static/v2ejvX3rw7GrlP26Ap25Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/366-11b6bc9ac6c34846c8c8.js","static/chunks/pages/index-b75686e976f7c6dc1cc5.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
